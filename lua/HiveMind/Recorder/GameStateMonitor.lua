20fd71cce5eb4ac0324cbb361e2112b86af86c17
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-12-14 16:07:01 +0000

    Add GameStateMonitor class to call back to HiveMindRecorder when a round state changes

---
+++ lua/HiveMind/Recorder/GameStateMonitor.lua
@@ -0,0 +1,57 @@
+--[[
+    Class to monitor the GameState.
+]]
+
+class 'GameStateMonitor'
+
+local lastState
+
+function GameStateMonitor:CheckGameState()
+    local currentState = GetGamerules():GetGameState()
+    if not lastState then
+        lastState = currentState
+        return false
+    end
+
+    if lastState ~= kGameState.Started then
+        self:CheckForGameStart(currentState)
+    end
+
+    self:CheckForGameEnd(currentState)
+
+    lastState = currentState
+
+    return currentState == kGameState.Started
+end
+
+function GameStateMonitor:CheckForGameStart(currentState)
+    if lastState ~= currentState then
+        if currentState == kGameState.Countdown then
+            self:OnCountdownStart()
+        elseif currentState == kGameState.Started then
+            self:OnGameStart()
+        end
+    end
+end
+
+function GameStateMonitor:CheckForGameEnd(currentState)
+    if lastState ~= currentState then
+        if currentState == kGameState.Team1Won or currentState == kGameState.Team2Won or currentState == kGameState.Draw then
+            self:OnGameEnd()
+            return true
+        end
+    end
+    return false
+end
+
+function GameStateMonitor:OnCountdownStart()
+    HiveMindRecorder:OnCountdownStart()
+end
+
+function GameStateMonitor:OnGameStart()
+    HiveMindRecorder:OnGameStart()
+end
+
+function GameStateMonitor:OnGameEnd()
+    HiveMindRecorder:OnGameEnd()
+end
