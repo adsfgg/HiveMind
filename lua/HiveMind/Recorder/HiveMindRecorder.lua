8c1a2db1d776cf59b584f94ae60dc2afb4f28c1e
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-12-14 17:06:28 +0000

    Implement game state monitor into HiveMindRecorder

--- lua/HiveMind/Recorder/HiveMindRecorder.lua
+++ lua/HiveMind/Recorder/HiveMindRecorder.lua
@@ -1,7 +1,35 @@
 if not Server then return end
 
+Script.Load("lua/HiveMind/Recorder/GameStateMonitor.lua");
+
 class 'HiveMindRecorder'
 
+local gameStateMonitor = nil
+
 function HiveMindRecorder:Initialize()
-    
+    gameStateMonitor = GameStateMonitor()
 end
+
+function HiveMindRecorder:OnCountdownStart()
+    HiveMindGlobals:SendChatMessage("Countdown start!")
+end
+
+function HiveMindRecorder:OnCountdownEnd()
+    HiveMindGlobals:SendChatMessage("Countdown end!")
+end
+
+function HiveMindRecorder:OnGameStart()
+    HiveMindGlobals:SendChatMessage("Game start!")
+end
+
+function HiveMindRecorder:OnGameEnd()
+    HiveMindGlobals:SendChatMessage("Game end!")
+end
+
+local function OnUpdateServer()
+    if gameStateMonitor:CheckGameState() then
+        --
+    end
+end
+
+Event.Hook("UpdateServer", OnUpdateServer)
