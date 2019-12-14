f56b6790eeb4cfad079aed9121b32af416902154
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-12-14 14:22:54 +0000

    start shared entrypoint

---
+++ lua/HiveMind/ModEntry/shared.lua
@@ -0,0 +1,20 @@
+HiveMindShared = {}
+
+-- Shared Variables
+HiveMindGlobals.version = "0.0.1"
+HiveMindGlobals.modName = "HiveMind"
+
+-- Shared Functions
+function HiveMindGlobals:SendChatMessage(msg, team, teamType)
+    if team == nil then
+        team = kTeamReadyRoom
+    end
+
+    if teamType == nil then
+        teamType = kNeutralTeamType
+    end
+
+    Server.SendNetworkMessage("Chat", BuildChatMessage(false, self.modName, -1, team, teamType, msg), true)
+    Shared.Message("Chat All - " .. self.modName .. ": " .. msg)
+    Server.AddChatToHistory(msg, self.modName, 0, team, false)
+end
