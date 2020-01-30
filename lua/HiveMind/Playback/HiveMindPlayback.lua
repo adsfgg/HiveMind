2955780d27543885aea071cb6fc90b8198783c1b
adsfgg <11364579+adsfgg@users.noreply.github.com>
2020-01-30 23:29:24 +0000

    New slate

--- lua/HiveMind/Playback/HiveMindPlayback.lua
+++ lua/HiveMind/Playback/HiveMindPlayback.lua
@@ -1,7 +1,5 @@
 if not Server then return end
 
--- Trackers
-Script.Load("lua/HiveMind/Trackers/TrackerManager.lua")
 -- Libraries
 Script.Load("lua/HiveMind/LibDeflate.lua")
 Script.Load("lua/HiveMind/base64.lua")
@@ -22,15 +20,10 @@ HiveMindPlayback.currentUpdate = 1
 function HiveMindPlayback:Initialize(demo_id)
     assert(demo_id ~= nil, "No demo id given")
 
-    self.trackerManager = TrackerManager()
-    self.trackerManager:Initialize()
-
     self.data = self:LoadData(demo_id)
 
     Event.Hook("ClientConnect", function(client) self:OnClientConnect(client) end)
     Event.Hook("UpdateServer", function(server) self:OnUpdateServer(server) end)
-
-    HiveMindGlobals:PrintDebug("Waiting for client to connect...")
 
     return self
 end
@@ -41,23 +34,16 @@ function HiveMindPlayback:OnClientConnect(client)
     assert(player)
 
     -- Force the connecting local player to spectate
-    HiveMindGlobals:PrintDebug("Client connecting...")
     if client:GetIsLocalClient() then
-        HiveMindGlobals:PrintDebug("Client is local, moving to spectate")
         GetGamerules():JoinTeam(player, kSpectatorIndex)
         HiveMindGlobals:SendChatMessage("Starting demo in 10 seconds")
 
         self.timeToStart = Shared.GetTime() + 10
-    else
-        HiveMindGlobals:PrintDebug("Client is not local, ignoring")
     end
 end
 
 function HiveMindPlayback:LoadData(demo_id)
-    HiveMindGlobals:PrintDebug("Attempting to open demo file")
     local dataFile = assert(io.open("config://HiveMind/" .. demo_id .. ".demo", "r"))
-
-    HiveMindGlobals:PrintDebug("Attempting to decode data")
 
     local data = dataFile:read()
     data = self.B64.decode(data)
@@ -65,26 +51,8 @@ function HiveMindPlayback:LoadData(demo_id)
     data = json.decode(data)
 
     assert(data ~= nil, "Failed to load demo")
-    HiveMindGlobals:PrintDebug("Demo data loaded successfully")
     return data
 end
 
 function HiveMindPlayback:OnUpdateServer(server)
-    if not self.playbackStarted then 
-        if self.timeToStart == 0 or Shared.GetTime() < self.timeToStart then
-            return
-        end
-
-        HiveMindGlobals:SendChatMessage("Starting demo")
-
-        self.playbackStarted = true
-
-        HiveMindGlobals:PrintDebug("Loading initial data")
-        self.trackerManager:UpdateAllTrackers_Playback(self.data["initial_data"])
-        return
-    end
-
-    self.trackerManager:UpdateAllTrackers_Playback(self.data["update_data"][self.currentUpdate])
-
-    self.currentUpdate = self.currentUpdate + 1
 end
